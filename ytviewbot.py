2d489bf71d69e46a0189cb35dedc695b2d3bff03
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-02-16 18:16:26 +0000

    Update ytviewbot.py

--- ytviewbot.py
+++ ytviewbot.py
@@ -15,8 +15,9 @@ delay = input("Enter Time For The Next View (atleast 10 secs): ")
 print("Be Careful,Your Computer Might Crash Depending On How Many Views You Want!")
 print("Ctrl + C To Stop!")
 
-chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
+#chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
 
 for i in range(int(views)):
-    webbrowser.get(chrome_path).open_new(url)
+    #webbrowser.get(chrome_path).open_new(url)
+    webbrowser.visit(url)
     time.sleep(int(delay))
