7a808b82bb76cc6d32b7c6842a7bef526e33c654
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-02-17 13:00:45 +0000

    Update ytviewbot.py

--- ytviewbot.py
+++ ytviewbot.py
@@ -1,7 +1,7 @@
 import webbrowser
 import time
 import pyfiglet
-import os
+import platform
 
 ascii_banner = pyfiglet.figlet_format("ViewBot")
 print(ascii_banner)
@@ -12,13 +12,20 @@ url = input("Enter The Link Of The Video: ")
 views = input("Enter The Amount Of Views You Want: ")
 delay = input("Enter How Long To Watch It (atleast 10 secs): ")
 
-
+print("")
+print("Next Video In",delay,"Seconds And Giving",views,"views")
 print("Be Careful,Your Computer Might Crash Depending On How Many Views You Want!")
 print("Ctrl + C To Stop!")
 
 
-chrome = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
+chrome_win = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
+
 
 for i in range(int(views)):
-        webbrowser.get(chrome).open(url)
-        time.sleep(int(delay))
+        
+        if platform.system() == 'Windows':
+                webbrowser.get(chrome_win).open(url)
+                time.sleep(int(delay))
+        else:
+                webbrowser.open(url)
+                time.sleep(int(delay))
