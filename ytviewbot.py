762f978a017b0e7541901a50b428a7e554c67ea7
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-02-22 11:12:45 +0000

    Update ytviewbot.py

--- ytviewbot.py
+++ ytviewbot.py
@@ -2,6 +2,7 @@ import webbrowser
 import time
 import pyfiglet
 import platform
+import os
 
 ascii_banner = pyfiglet.figlet_format("ViewBot")
 print(ascii_banner)
@@ -10,22 +11,40 @@ print("					By CxllZ")
 
 url = input("Enter The Link Of The Video: ")
 views = input("Enter The Amount Of Views You Want: ")
-delay = input("Enter How Long To Watch It (atleast 10 secs): ")
+delay = input("Enter Minimum Time To Watch (seconds): ")
+brow = input("Enter Your Default Browser : ")
 
 print("")
-print("Next Video In",delay,"Seconds And Giving",views,"views")
-print("Be Careful,Your Computer Might Crash Depending On How Many Views You Want!")
-print("Ctrl + C To Stop!")
-
+print("Is The Following Details Correct?")
+print("")
+print("url:",url)
+print("views:",views)
+print("Watch Time:",delay,"seconds")
+print("browser:",brow)
+print("")
 
 chrome_win = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
 
 
-for i in range(int(views)):
-        
-        if platform.system() == 'Windows':
+while True:
+    correct = input("y/n: ")
+    if correct == '' or not correct in ['y','n']: 
+        print('Please answer with y or n!') 
+    else: 
+        break
+    
+if correct == 'y':
+    for i in range(int(views)):
+            if platform.system() == 'Windows':
+                print("Successfully Viwed. ")
+                os.system(" killall -9 " + brow)
                 webbrowser.get(chrome_win).open(url)
-                time.sleep(int(delay))
-        else:
+                time.sleep(int(minimum))
+            else:
+                print("Successfully Viwed. ")
+                os.system("taskkill /im",brow,"/")
                 webbrowser.open(url)
-                time.sleep(int(delay))
+                time.sleep(int(minimum))
+if correct == 'n':
+    print("RESTARTING...")
+    os.execv(sys.executable, ['python'] + sys.argv)
